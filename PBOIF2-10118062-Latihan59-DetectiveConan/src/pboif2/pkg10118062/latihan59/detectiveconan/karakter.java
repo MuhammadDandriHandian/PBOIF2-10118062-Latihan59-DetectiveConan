b6f741d524e22736593c144b0353276c57431c0a
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-11-08 00:16:08 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan59-DetectiveConan/src/pboif2/pkg10118062/latihan59/detectiveconan/karakter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan59.detectiveconan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class karakter {
+    public String nama,ciri;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getCiri() {
+        return ciri;
+    }
+
+    public void setCiri(String ciri) {
+        this.ciri = ciri;
+    }
+    
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+  public void tampil(){
+      
+       
+    }
+  public  void ciri(){
+      
+  }
+}
---
+++ PBOIF2-10118062-Latihan59-DetectiveConan/src/pboif2/pkg10118062/latihan59/detectiveconan/nama4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan59.detectiveconan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class nama4 extends karakter {
+
+    @Override
+    public void tampil(){
+        System.out.println("nama :"+nama);
+    }
+    @Override
+        public void ciri(){
+        System.out.println("deskripsi : Sonoko Suzuki sahabat terbaik Ran."
+                + " Sonoko berasal dari keluarga kaya dengan status sosial yang tinggi");
+        System.out.println("");
+    }
+}
